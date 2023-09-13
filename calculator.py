cbe743a2d0091d2feed56a488fbc16664e273fa5
Himanshuupraity <144529158+Himanshuupraity@users.noreply.github.com>
2023-09-13 15:07:57 +0530

    Add files via upload
    Python program of basic calculator.

---
+++ calculator.py
@@ -0,0 +1,20 @@
+print("1.add")
+print("2.sub")
+print("3.mul")
+print("4.div")
+
+x=int(input("enter the choice"))
+a=int(input("enter the first value"))
+b=int(input("enter the second value"))
+if(x==1):
+    c=a+b
+    print(c)
+elif(x==2):
+    c=a-b
+    print(c)
+elif(x==3):
+    c=a*b
+    print(c)
+elif(x==4):
+    c=a/b
+    print(c)
