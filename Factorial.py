2cbf2d71620b4e926298cf9f9e1ec4ffeaa04ae8
Himanshuupraity <144529158+Himanshuupraity@users.noreply.github.com>
2023-09-18 15:12:44 +0530

    Add files via upload
    Basic Factorial Program

---
+++ Factorial.py
@@ -0,0 +1,13 @@
+# a=int(input("Enter the number: "))   
+# fact=1
+# for i in range (fact,a+1):
+#     fact=fact*i
+# print("Factorial is : ", fact) 
+
+
+
+a=int(input("Enter the number: "))     
+fact=1
+for i in range (1,a+1):
+    fact=fact*i
+print("Factorial is : ", fact) 
